ceaf4ea8c2fee1df658b02b663ebd3c255e66387
adarshdev <Adarshkatoch82@gmail.com>
2025-07-15 19:29:11 +0530

    Update testimonials

--- src/components/Card/TestimonialCard.tsx
+++ src/components/Card/TestimonialCard.tsx
@@ -10,7 +10,7 @@ export const TestimonialCard = ({ testimonial }: { testimonial: any }) => {
 
   return (
     <div className="rounded-xl border border-[#C0DBF7] p-6 shadow-md h-full flex flex-col justify-between text-center bg-white">
-      {photoUrl ? (
+      {/* {photoUrl && (
         <div className="mx-auto w-24 h-24 rounded-full overflow-hidden mb-4">
           <Image
             width={100}
@@ -20,17 +20,16 @@ export const TestimonialCard = ({ testimonial }: { testimonial: any }) => {
             className="w-full h-full object-cover"
           />
         </div>
-      ) : (
-        <div className="mx-auto w-24 h-24 rounded-full overflow-hidden mb-4">
-          <Image
-            width={100}
-            height={100}
-            src={defaultImg}
-            alt={testimonial.name}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      )}
+      )} */}
+      <div className="mx-auto w-24 h-24 rounded-full overflow-hidden mb-4">
+        <Image
+          width={100}
+          height={100}
+          src={defaultImg}
+          alt={testimonial.name}
+          className="w-full h-full object-cover"
+        />
+      </div>
 
       <div className="flex flex-col gap-2">
         <p className="text-[#3D3D3D] text-sm italic">{testimonial.quote}</p>
